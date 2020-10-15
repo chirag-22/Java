f385848775329504a26e0536006813a0b25d09ac
chirag-22 <53221745+chirag-22@users.noreply.github.com>
2020-10-15 11:04:29 +0530

    Update try.php

--- Innovation/try.php
+++ Innovation/try.php
@@ -1,16 +1,16 @@
 <html>
 <head>
 <link rel="stylesheet"
-	href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    href="https://fonts.googleapis.com/icon?family=Material+Icons">
 <meta name="google-signin-scope" content="profile email">
 <link rel="stylesheet"
-	href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
-	integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z"
-	crossorigin="anonymous">
+    href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+    integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z"
+    crossorigin="anonymous">
 
 <meta name="google-signin-scope" content="profile email">
 <meta name="google-signin-client_id"
-	content="1092644277096-fr7rjfullv9u6jgk56hi64eje3q4tg7s.apps.googleusercontent.com">
+    content="1092644277096-fr7rjfullv9u6jgk56hi64eje3q4tg7s.apps.googleusercontent.com">
 <script src="https://apis.google.com/js/platform.js" async defer></script>
 
 <script class="external"></script>
@@ -20,8 +20,12 @@
 <body>
 <div class="row mt-5 pt-5">
 <div class="col-sm-6 text-right">
-<div class="g-sharetoclassroom" data-size="60" data-url="..." ></div>
-</div>
+<!--<div class="g-sharetoclassroom" data-size="60" data-url="..." ></div>
+<button> <a href="https://drive.google.com/drive/mobile/folders/1ybOvW-UkQmpyKUyUqT1_Jg6BvP9sIjxh?ogsrc=32&tab=mo&sort=13&direction=a" >Google Drive</a></button>
+</div> -->
+<button> <a href="https://classroom.google.com/c/MTgwMDQxMDY2OTQ4?cjc=x7pcbkv" >Google Classroom</a></button>
+<button> <a href="https://drive.google.com/drive/my-drive" >Google Drive</a></button>
+
 <script >
   window.___gcfg = {
     parsetags: 'onload'
@@ -30,3 +34,4 @@
 <script src="https://apis.google.com/js/platform.js" async defer></script>
 
 </body>
+</html>
